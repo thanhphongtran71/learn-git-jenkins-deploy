f8b3d7ec09d801bfcddfc2a6fe3ddd402dea1ac5
thanhphongtran71 <thanhphongtran42@gmail.com>
2024-03-31 09:52:02 +0700

    demo deploy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("haha");
+		System.out.println("hello world");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
